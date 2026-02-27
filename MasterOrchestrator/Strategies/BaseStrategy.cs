6b19b014a17000a7907046e74ebdfa1f308ea106
Burabari <burabaridev@gmail.com>
2026-02-27 01:53:06 +0100

    create BaseStrategy abstract class

---
+++ MasterOrchestrator/Strategies/BaseStrategy.cs
@@ -0,0 +1,28 @@
+using cAlgo.API;
+using cTraderV1.Core;
+using cTraderV1.Models;
+
+namespace cTraderV1.Strategies
+{
+    public abstract class BaseStrategy : ITradingStrategy
+    {
+        protected readonly Robot Bot;
+        public abstract string Name { get; }
+        public StrategyState CurrentState { get; protected set; } = StrategyState.Idle;
+
+        protected BaseStrategy(Robot robot)
+        {
+            this.Bot = robot;
+        }
+
+        public abstract void Initialize();
+        public abstract void OnBar(SignalCollection signals);
+        public abstract void OnTick();
+
+        public virtual void Reset()
+        {
+            CurrentState = StrategyState.Idle;
+            Bot.Print($"{Name} reset to Idle.");
+        }
+    }
+}
